18860b2424d7bb866e76aa9c0c3825a120471817
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-06-05 18:47:11 +0200

    filtering improvements

--- components/FilterMenu.tsx
+++ components/FilterMenu.tsx
@@ -6,16 +6,40 @@ import {
   Select,
   IconButton,
 } from "@chakra-ui/react";
-import { FC, FormEvent } from "react";
-import { MdSearch } from "react-icons/md";
+import { FC, FormEvent, useState } from "react";
+import { MdCancel, MdSearch } from "react-icons/md";
 import { QueryVars } from "../queries/characters";
+
+type StatusType = "alive" | "dead" | "unknown";
+type GenderType = "female" | "male" | "genderless" | "unknown";
 
 // eslint-disable-next-line no-unused-vars
 const FilterMenu: FC<{ onSubmit: (filters: QueryVars) => void }> = ({
   onSubmit,
 }) => {
+  const [name, setName] = useState<string>();
+  const [status, setStatus] = useState<StatusType>();
+  const [species, setSpecies] = useState<string>();
+  const [type, setType] = useState<string>();
+  const [gender, setGender] = useState<GenderType>();
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    onSubmit({
+      name,
+      status,
+      species,
+      type,
+      gender,
+    });
+  }
+
+  function clearFilters() {
+    setName(undefined);
+    setStatus(undefined);
+    setSpecies(undefined);
+    setType(undefined);
+    setGender(undefined);
     onSubmit({});
   }
 
@@ -34,11 +58,22 @@ const FilterMenu: FC<{ onSubmit: (filters: QueryVars) => void }> = ({
       >
         <FormControl>
           <FormLabel htmlFor="name">Character name</FormLabel>
-          <Input id="name" type="text" placeholder="E.g. Morty" />
+          <Input
+            id="name"
+            type="text"
+            placeholder="E.g. Morty"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </FormControl>
         <FormControl>
           <FormLabel htmlFor="status">Status</FormLabel>
-          <Select id="status" placeholder="Select option">
+          <Select
+            id="status"
+            placeholder="Select option"
+            value={status}
+            onChange={(e) => setStatus(e.target.value as StatusType)}
+          >
             <option value="alive">Alive</option>
             <option value="dead">Dead</option>
             <option value="unknown">Unknown</option>
@@ -46,15 +81,32 @@ const FilterMenu: FC<{ onSubmit: (filters: QueryVars) => void }> = ({
         </FormControl>
         <FormControl>
           <FormLabel htmlFor="species">Species</FormLabel>
-          <Input id="species" type="text" placeholder="E.g. alien" />
+          <Input
+            id="species"
+            type="text"
+            placeholder="E.g. alien"
+            value={species}
+            onChange={(e) => setSpecies(e.target.value)}
+          />
         </FormControl>
         <FormControl>
           <FormLabel htmlFor="type">Type</FormLabel>
-          <Input id="type" type="text" placeholder="E.g. parasite" />
+          <Input
+            id="type"
+            type="text"
+            placeholder="E.g. parasite"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          />
         </FormControl>
         <FormControl>
           <FormLabel htmlFor="gender">Gender</FormLabel>
-          <Select id="gender" placeholder="Select option">
+          <Select
+            id="gender"
+            placeholder="Select option"
+            value={gender}
+            onChange={(e) => setGender(e.target.value as GenderType)}
+          >
             <option value="female">Female</option>
             <option value="male">Male</option>
             <option value="genderless">Genderless</option>
@@ -66,11 +118,19 @@ const FilterMenu: FC<{ onSubmit: (filters: QueryVars) => void }> = ({
           aria-label="search"
           icon={<MdSearch />}
           variant="solid"
-          colorScheme="rnmMagenta"
+          colorScheme="green"
           fontSize="xl"
-        >
-          Search
-        </IconButton>
+        />
+        <IconButton
+          type="button"
+          aria-label="clear filters"
+          icon={<MdCancel />}
+          variant="outline"
+          colorScheme="rnmRed"
+          fontSize="xl"
+          disabled={!(name || status || species || type || gender)}
+          onClick={() => clearFilters()}
+        />
       </form>
     </Flex>
   );
